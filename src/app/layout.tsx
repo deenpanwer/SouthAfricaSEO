4598ecb9fed2a391b17e1b7b1fbed4df5ba6ae83
Kaif Khan <kaif.khan1834@gmail.com>
2025-06-11 05:31:23 +0000

    feat: Add favicon and update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: {
-    default: 'TRAC - Amplify Your Revenue & Sales',
+    default: 'TRAC provides strategic business solutions to increase your website traffic, leads, and sales, driving significant growth for your company.',
     template: '%s | TRAC',
   },
   description: 'TRAC provides strategic business solutions to increase your website traffic, leads, and sales, driving significant growth for your company.',
@@ -50,6 +50,7 @@ export default function RootLayout({
         <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WFVWSV9V');` }} />
         {/* End Google Tag Manager */}
         {/* Meta Pixel Code */}
+        <link rel="icon" href="/favicon.ico" />
         <script dangerouslySetInnerHTML={{ __html: `
           !function(f,b,e,v,n,t,s)
           {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
