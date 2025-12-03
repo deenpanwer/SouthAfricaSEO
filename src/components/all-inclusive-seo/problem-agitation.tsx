503e74f7670aaf13c5645f53d4c749bbeb8c59f4
Kaif Khan <kaif.khan1834@gmail.com>
2025-12-03 21:24:27 +0000

    i mean just make its high a little less of that shadow

--- src/components/all-inclusive-seo/problem-agitation.tsx
+++ src/components/all-inclusive-seo/problem-agitation.tsx
@@ -134,7 +134,7 @@ const ProblemAgitation = () => {
           </div>
         </div>
       </motion.div>
-      <div className="absolute bottom-0 left-0 w-full h-32 bg-gradient-to-t from-black to-transparent pointer-events-none" />
+      <div className="absolute bottom-0 left-0 w-full h-20 bg-gradient-to-t from-black/50 to-transparent pointer-events-none" />
     </div>
   );
 };
